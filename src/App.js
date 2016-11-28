1b44534e5293d9e5b8820e7d5bf2cb02f1ebb0e9
Darell Koh <darellkoh@gmail.com>
2016-11-28 08:21:22 -0500

    api request successful

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import xhr from 'xhr';
 
 class App extends React.Component {
   state = {
-    location: ''
+    location: '',
+    data: {}
   };
 
   fetchData = (evt) => {
     evt.preventDefault();
-    const location = encodedURIComponent(this.state.location);
+    const location = encodeURIComponent(this.state.location);
+    console.log('location', this.state.location)
     const urlPrefix = "http://api.openweathermap.org/data/2.5/forecast?q=";
     const urlSuffix = "&APPID=7f885a80f78f49f408cc23272581f0d3&units=imperial";
     const url = urlPrefix + location + urlSuffix;
 
-    console.log('fetching data for: ', {this.state.location});
+    const self = this;
 
     xhr({
       url: url
       }, (err, data) => {
-
-      }
-    });
-
+        console.log('data', data);
+        self.setState({
+          data: JSON.parse(data.body)
+        });
+      });
   };
 
   changeLocation = (evt) => {
