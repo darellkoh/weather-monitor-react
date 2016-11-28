4337b51a8ffa0e83a6265bd1e4f821413ac49420
Darell Koh <darellkoh@gmail.com>
2016-11-28 17:03:56 -0500

    refactored using redux

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import './App.css';
 import xhr from 'xhr';
 import Plot from './plot.js';
+import {connect} from 'react-redux';
+import {changeLocation, setData, setDates, setTemps, setSelectedTemp, setSelectedDate} from './actions.js';
 
 class App extends React.Component {
-  state = {
-    location: '',
-    data: {},
-    dates: [],
-    temps: [],
-    selected: {
-      date: '',
-      temp: null
-    }
-  };
+  // state = {
+  //   return {
+  //     data: {},
+  //     dates: [],
+  //     temps: [],
+  //   };
+  // };
 
   fetchData = (evt) => {
     evt.preventDefault();
-    const location = encodeURIComponent(this.state.location);
+    const location = encodeURIComponent(this.props.location);
     const urlPrefix = "http://api.openweathermap.org/data/2.5/forecast?q=";
     const urlSuffix = "&APPID=7f885a80f78f49f408cc23272581f0d3&units=imperial";
     const url = urlPrefix + location + urlSuffix;
@@ -37,64 +36,61 @@ class App extends React.Component {
           temps.push(element.main.temp)
         }
 
-        self.setState({
-          data: body,
-          dates: dates,
-          temps: temps,
-          selected: {
-            date: '',
-            temp: null
-          }
-        });
+        // self.setState({
+        //   data: data,
+        //   dates: dates,
+        //   temps: temps,
+        // });
+        self.props.dispatch(setData(body));
+        self.props.dispatch(setDates(dates));
+        self.props.dispatch(setTemps(temps));
+        self.props.dispatch(setSelectedTemp(null));
+        self.props.dispatch(setSelectedDate(''));
       });
   };
 
+
   changeLocation = (evt) => {
-    this.setState({
-      location: evt.target.value
-    });
+    this.props.dispatch(changeLocation(evt.target.value));
   }
 
   onPlotClick = (data) => {
     if(data.points){
-      this.setState({
-        selected: {
-          date: data.points[0].x,
-          temp: data.points[0].y
-        }
-      })
-
+      var number = data.points[0].pointNumber;
+      this.props.dispatch(setSelectedDate(this.props.dates[number]));
+      this.props.dispatch(setSelectedTemp(this.props.temps[number]));
     }
-  }
+  };
 
   render() {
     let currentTemp;
-    if(this.state.data.list){
-      currentTemp = this.state.data.list[0].main.temp;
+    if(this.props.data.list){
+      currentTemp = this.props.data.list[0].main.temp;
     }
 
     return (
     <div>
       <h1>Weather Monitor</h1>
-        <form onSubmit={ this.fetchData }>
+        <form onSubmit={this.fetchData}>
           <label>I want to know the weather for
-            <input placeholder={ "City, Country" } type="text"
-            value={ this.state.location }
-            onChange={ this.changeLocation }
+            <input placeholder={"City, Country"}
+            type="text"
+            value={this.props.location}
+            onChange={this.changeLocation}
             />
           </label>
         </form>
-        {(this.state.data.list) ? (
-          <div className="wrapper">
-              <p className="temp-wrapper">
-                <span className="temp">{ this.state.selected.temp ? this.state.selected.temp : currentTemp }</span>
-                <span className="temp-symbol">°F</span>
-                <span className="temp-date">{ this.state.selected.temp ? this.state.selected.date : '' }</span>
-              </p>
+        {(this.props.data.list) ? (
+          <div className='wrapper'>
+          {(this.props.selected.temp) ? (
+            <p className='temp-wrapper'>The temperature on {this.props.selected.date} will be {this.props.selected.temp}°F</p>
+            ) : (
+            <p className='temp-wrapper'>The current temperature is {currentTemp}°F!</p>
+            )}
               <h2>Forecast</h2>
                 <Plot
-                xData={ this.state.dates }
-                yData={ this.state.temps }
+                xData={ this.props.dates }
+                yData={ this.props.temps }
                 type="scatter"
                 onPlotClick={ this.onPlotClick }
                 />
@@ -105,6 +101,9 @@ class App extends React.Component {
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  return state;
+}
 
+export default connect(mapStateToProps)(App);
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import App from './App.js';
+import mainReducer from './reducers.js';
 import './index.css';
 
+const store = createStore(mainReducer);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
